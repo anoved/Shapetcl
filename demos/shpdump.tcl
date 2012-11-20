ef92a1082b2cb9822322ab035c63a45cb412903e
Jim DeVona <jim.devona@gmail.com>
2012-11-20 17:35:52 -0500

    Tidied shpdump demo

--- demos/shpdump.tcl
+++ demos/shpdump.tcl
@@ -9,21 +9,24 @@ if {$argc != 1} {
 }
 
 set shp [shapefile [lindex $argv 0]]
-puts [format "File type: %s" [$shp type]]
+puts "File type: [$shp type]"
 
 set fcount [$shp count]
-puts [format "Features in file: %d" $fcount]
+puts "Features in file: $fcount"
 
+# iterate through all features
 for {set fid 0} {$fid < $fcount} {incr fid} {
 	set feature [$shp coords $fid -all]
 	set pcount [llength $feature]
 	puts "\nFeature $fid (parts: $pcount):"
 	
+	# iterate through all parts of this feature
 	for {set partid 0} {$partid < $pcount} {incr partid} {
 		set part [lindex $feature $partid]
 		set vcount [expr {[llength $part] / 4}]
 		puts " Part $partid (vertices: $vcount):"
 		
+		# print all vertices of this feature part
 		foreach {x y z m} $part {
 			puts "  X: $x, Y: $y, Z: $z, M: $m"
 		}
