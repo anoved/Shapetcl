ec94c990cde6067350fd9acbff4a9eb1f41cdd6f
Jim DeVona <jim.devona@gmail.com>
2012-11-22 19:21:07 -0500

    Configure test output to be more verbose runAllTest blarfs config info anyway, so might as well include more informative test progress messages.

--- tests/all.tcl
+++ tests/all.tcl
@@ -8,7 +8,13 @@ package require tcltest 2
 # runAllTests should only look here, in the tests directory, for tests.
 # It should not bother looking for tests in the tmp subdirectory.
 # Files that end in .test.tcl (excepting any lock files) contain tests.
-::tcltest::configure -tmpdir tmp -testdir [::tcltest::workingDirectory] -asidefromdir {tmp sample} -file {*.test.tcl} -notfile {l.*.test.tcl}
+::tcltest::configure \
+		-tmpdir tmp \
+		-testdir [::tcltest::workingDirectory] \
+		-asidefromdir {tmp sample} \
+		-file {*.test.tcl} \
+		-notfile {l.*.test.tcl} \
+		-verbose {body pass skip error}
 
 # Apply any additional configuration arguments.
 eval ::tcltest::configure $argv
